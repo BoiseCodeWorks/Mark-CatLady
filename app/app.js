6105d559c490a088a9c0b0c61fac0e9021ea0cd4
Mark Ohnsman <mark.ohnsman@gmail.com>
2019-02-07 10:56:22 -0700

    clair

--- app/app.js
+++ app/app.js
@@ -3,12 +3,23 @@ let cat = {
     moods: ['happy', 'bitey', 'hiding'],
     tolerance: 3,
     moodImgs: ['./assets/fancy-cat.jpg', './assets/angry-cat.jpg', './assets/ran-away-cat.jpg'],
-    pets: 0
+    pets: 0,
+    moodIndex: 0
 }
 
 function pet() {
     cat.pets++
     if (cat.pets > cat.tolerance * 2) {
-        //update page
-    } else if 
+        cat.moodIndex = 2
+    } else if (cat.pets > cat.tolerance) {
+        cat.moodIndex = 1
+    }
+    draw()
+}
+
+function draw() {
+    document.getElementById('fancy-cat').src = cat.moodImgs[cat.moodIndex]
+    document.getElementById('pets').innerText = cat.pets
+    document.getElementById('mood').innerText = cat.moods[cat.moodIndex]
+
 }
