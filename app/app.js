419b3e798d43d6fb51d10c67611b0c9c2f372ec2
Mark Ohnsman <mark.ohnsman@gmail.com>
2019-02-07 11:15:37 -0700

    Jake

--- app/app.js
+++ app/app.js
@@ -4,11 +4,13 @@ let cat = {
     tolerance: 3,
     moodImgs: ['./assets/fancy-cat.jpg', './assets/angry-cat.jpg', './assets/ran-away-cat.jpg'],
     pets: 0,
-    moodIndex: 0
+    moodIndex: 0,
+    totalPets: 0
 }
 
 function pet() {
     cat.pets++
+    cat.totalPets++
     if (cat.pets > cat.tolerance * 2) {
         cat.moodIndex = 2
     } else if (cat.pets > cat.tolerance) {
@@ -19,7 +21,16 @@ function pet() {
 
 function draw() {
     document.getElementById('fancy-cat').src = cat.moodImgs[cat.moodIndex]
-    document.getElementById('pets').innerText = cat.pets
+    document.getElementById('pets').innerText = cat.totalPets
     document.getElementById('mood').innerText = cat.moods[cat.moodIndex]
+    if (cat.pets > cat.tolerance * 2) {
+        document.querySelector('.btn-info').disabled = true
+    }
+}
 
+function reset() {
+    cat.pets = 0
+    cat.moodIndex = 0
+    document.querySelector('.btn-info').disabled = false
+    draw()
 }
