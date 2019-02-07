c9173dfe6453afa7e64c215cf6fd7a1b6bffb98f
Mark Ohnsman <mark.ohnsman@gmail.com>
2019-02-07 11:52:36 -0700

    multi cats

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-let cat = {
+let cats = [{
     name: 'mr Snibbley',
     moods: ['happy', 'bitey', 'hiding'],
     tolerance: 3,
@@ -6,31 +6,71 @@ let cat = {
     pets: 0,
     moodIndex: 0,
     totalPets: 0
-}
+}, {
+    name: 'Tom',
+    moods: ['happy', 'punchy', 'hiding'],
+    tolerance: 5,
+    moodImgs: ['https://i.pinimg.com/originals/92/a6/2f/92a62f0221f58fe503a15fcb13f5c107.png', 'https://d2w9rnfcy7mm78.cloudfront.net/1074239/original_6062f9b67626611e1f0b14a8e1bf9059.png?1497401369', 'https://data.whicdn.com/images/304581024/large.jpg?t=1515030870'],
+    pets: 0,
+    moodIndex: 0,
+    totalPets: 0
+}]
 
-function pet() {
-    cat.pets++
-    cat.totalPets++
-    if (cat.pets > cat.tolerance * 2) {
-        cat.moodIndex = 2
-    } else if (cat.pets > cat.tolerance) {
-        cat.moodIndex = 1
+let activeCatIndex = 0
+
+function pet(petVal) {
+    cats[activeCatIndex].pets += petVal
+    cats[activeCatIndex].totalPets++
+    if (cats[activeCatIndex].pets > cats[activeCatIndex].tolerance * 2) {
+        cats[activeCatIndex].moodIndex = 2
+    } else if (cats[activeCatIndex].pets > cats[activeCatIndex].tolerance) {
+        cats[activeCatIndex].moodIndex = 1
     }
     draw()
 }
 
 function draw() {
-    document.getElementById('fancy-cat').src = cat.moodImgs[cat.moodIndex]
-    document.getElementById('pets').innerText = cat.totalPets
-    document.getElementById('mood').innerText = cat.moods[cat.moodIndex]
-    if (cat.pets > cat.tolerance * 2) {
+    document.getElementById('name').innerText = cats[activeCatIndex].name
+    document.getElementById('fancy-cat').src = cats[activeCatIndex].moodImgs[cats[activeCatIndex].moodIndex]
+    document.getElementById('pets').innerText = cats[activeCatIndex].totalPets
+    document.getElementById('mood').innerText = cats[activeCatIndex].moods[cats[activeCatIndex].moodIndex]
+    if (cats[activeCatIndex].pets > cats[activeCatIndex].tolerance * 2) {
         document.querySelector('.btn-info').disabled = true
     }
 }
 
 function reset() {
-    cat.pets = 0
-    cat.moodIndex = 0
+    cats[activeCatIndex].pets = 0
+    cats[activeCatIndex].moodIndex = 0
     document.querySelector('.btn-info').disabled = false
     draw()
 }
+
+function setActiveCat(index) {
+    activeCatIndex = index
+    draw()
+}
+
+
+
+
+
+
+// let winner = {
+//     rock: ['scissors', 'lizard'],
+//     paper: ['rock', 'spock'],
+//     scissors: ['paper', 'lizard'],
+//     lizard: ['spock', 'paper'],
+//     spock: ['scissors', 'rock']
+// }
+
+
+// function winner(pChoice, cChoice) {
+//     if (pChoice == cChoice) {
+//         //tie
+//     } else if (winner[pChoice].includes(cChoice)) {
+//         //winner
+//     } else {
+//         //loser
+//     }
+// }
