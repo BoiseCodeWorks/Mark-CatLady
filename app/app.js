47748dfc51e927aff476a460d6bf8c284cd69213
Mark Ohnsman <mark.ohnsman@gmail.com>
2019-02-07 10:46:35 -0700

    sean

---
+++ app/app.js
@@ -0,0 +1,14 @@
+let cat = {
+    name: 'mr Snibbley',
+    moods: ['happy', 'bitey', 'hiding'],
+    tolerance: 3,
+    moodImgs: ['./assets/fancy-cat.jpg', './assets/angry-cat.jpg', './assets/ran-away-cat.jpg'],
+    pets: 0
+}
+
+function pet() {
+    cat.pets++
+    if (cat.pets > cat.tolerance * 2) {
+        //update page
+    } else if 
+}
